d3a81809c5516ab84fa7ea3a90205f58d6990330
Eistiak Ahmed <eistiakahmedmeraj@gmail.com>
2025-10-08 05:52:55 +0600

    fetch product page data, implement search functionality, and complete product page design

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
+import useApps from '../../Hooks/useApps';
+import AppPage from '../AppPage/AppPage';
 
 const Apps = () => {
+  const { apps } = useApps();
+  const [search, setSearch] = useState('')
+
+
+   const term = search.trim().toLocaleLowerCase()
+
+   const searchApps = term ? apps.filter(app => app.title.toLocaleLowerCase().includes(term)): apps
+  
+
+
   return (
-    <div>
-        <h1 className='text-center text-4xl font-bold'>Apps</h1>
+    <div className="w-11/12 mx-auto">
+      <div className="text-center space-y-2">
+        <h1 className="text-5xl font-bold ">Our All Applications</h1>
+        <p className="lg:text-xl text-[#627382]">
+          Explore All Apps on the Market developed by us. We code for Millions
+        </p>
+      </div>
+      <div className='flex justify-between items-center mt-10 mb-4'>
+        <p className="text-2xl font-semibold">({searchApps.length}) Apps Found</p>
+        <div>
+          <label className="input">
+            <svg
+              className="h-[1em] opacity-50"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+            >
+              <g
+                strokeLinejoin="round"
+                strokeLinecap="round"
+                strokeWidth="2.5"
+                fill="none"
+                stroke="currentColor"
+              >
+                <circle cx="11" cy="11" r="8"></circle>
+                <path d="m21 21-4.3-4.3"></path>
+              </g>
+            </svg>
+            <input onChange={(e)=> setSearch(e.target.value)} type="search" required placeholder="search Apps" />
+          </label>
+        </div>
+      </div>
+
+      <div className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-5 lg:gap-7 mt-5'>
+        {
+          searchApps.map(app => <AppPage key={app.id} app={app}/>)
+        }
+      </div>
     </div>
   );
 };
 
-export default Apps; 
+export default Apps;
